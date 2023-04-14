990391f81eca90a2010c05b543f29842ffef2f7f
skgndi12 <skgndi123@naver.com>
2023-04-14 19:29:22 +0900

    feat: Implement initialize logger function

--- api/src/logger/logger.ts
+++ api/src/logger/logger.ts
@@ -24,3 +24,44 @@ const customLogLevelOption = {
     debug: 4
   }
 };
+
+let logger: customLogger;
+
+export function initialize(config: loggerFormat) {
+  let loggerOption: LoggerOptions;
+  if (config.format === LogFormat.JSON) {
+    loggerOption = jsonLoggerConfig();
+  } else {
+    loggerOption = textLoggerConfig();
+  }
+  logger = createLogger(loggerOption) as customLogger;
+}
+
+export function L(): customLogger {
+  return logger;
+}
+
+function textLoggerConfig(): LoggerOptions {
+  return {
+    levels: customLogLevelOption.levels,
+    level: 'info',
+    format: format.combine(
+      format.splat(),
+      format.simple(),
+      format.timestamp(),
+      format.printf(({ level, message, timestamp }) => {
+        return `[${timestamp}][${level}]: ${message}`;
+      })
+    ),
+    transports: [new transports.Console()]
+  };
+}
+
+function jsonLoggerConfig(): LoggerOptions {
+  return {
+    levels: customLogLevelOption.levels,
+    level: 'info',
+    format: format.combine(format.splat(), format.timestamp(), format.json()),
+    transports: [new transports.Console()]
+  };
+}
