9f46d8f3d10c61c97dc126c6a7e827e574abb42d
skgndi12 <skgndi123@naver.com>
2023-04-14 19:30:19 +0900

    feat: Add initialize logger code

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,5 +1,12 @@
-console.log('Hello World!')
+import * as logger from './logger/logger';
+import { LogFormat } from './config/enum';
+
+function main() {
+  const loggerFormat: logger.loggerFormat = { format: LogFormat.TEXT };
+  logger.initialize(loggerFormat);
+  logger.L().info('Hello World %s', 'From Mr.C');
+}
 
 if (require.main === module) {
-    main();
+  main();
 }
