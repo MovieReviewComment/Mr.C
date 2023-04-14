f90fc7d15a49be1aa0ad9363d735084230110d21
skgndi12 <skgndi123@naver.com>
2023-04-14 19:28:11 +0900

    feat: Add custom logger type and level option

--- api/src/logger/logger.ts
+++ api/src/logger/logger.ts
@@ -1,5 +1,26 @@
+import {
+  LoggerOptions,
+  Logger,
+  LeveledLogMethod,
+  createLogger,
+  format,
+  transports
+} from 'winston';
 import { LogFormat } from '../config/enum';
 
 export type loggerFormat = {
   format: LogFormat;
+};
+
+type customLogger = Logger &
+  Record<keyof (typeof customLogLevelOption)['levels'], LeveledLogMethod>;
+
+const customLogLevelOption = {
+  levels: {
+    fatal: 0,
+    error: 1,
+    warn: 2,
+    info: 3,
+    debug: 4
+  }
 };
