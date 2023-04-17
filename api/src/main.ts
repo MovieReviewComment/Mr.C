23672797b98277b5bf5bf017a79cb73fcdc7a907
skgndi12 <skgndi123@naver.com>
2023-04-17 18:34:33 +0900

    ✨ Initialize logger with supported format

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,5 +1,12 @@
+import { LogFormat } from './config/enum';
+import * as logger from './logger/logger';
+
 function main() {
+  const loggerFormat: logger.loggerFormat = { format: LogFormat.TEXT };
+  logger.initialize(loggerFormat);
+  logger.L().info('Hello World %s', 'From Mr.C');
 }
+
 if (require.main === module) {
   main();
 }
