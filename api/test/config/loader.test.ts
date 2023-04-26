886be2ecf6eb95767a225a2254bf4abb20e18b9a
skgndi12 <skgndi123@naver.com>
2023-04-26 18:47:51 +0900

    ✅ Rename tests and change order of execution

--- api/test/config/loader.test.ts
+++ api/test/config/loader.test.ts
@@ -2,16 +2,16 @@ import { load } from '../../src/config/loader';
 import config from 'config';
 
 describe('Test config loader', () => {
-  it('should loading configurations from a test.yaml file is correct', () => {
+  it('should load valid configurations from a test.yaml', () => {
     expect(load()).toStrictEqual({
       env: 'test',
       http: { host: '127.0.0.1', port: 10100 }
     });
   });
 
-  it('should loading configurations from a test.yaml and does not overriden by env variable', () => {
-    const config = load();
+  it('should not override fields by env variable', () => {
     process.env.env = 'production';
+    const config = load();
     expect(config.env).not.toEqual('production');
   });
 
