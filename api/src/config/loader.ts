32258078498a026173ec5083682c6d2bb1e2b9bb
skgndi12 <skgndi123@naver.com>
2023-05-03 19:27:53 +0900

    🏷️ Specify logger format by config vaue

--- api/src/config/loader.ts
+++ api/src/config/loader.ts
@@ -1,20 +1,26 @@
+import { LogFormat } from '@src/config/enum';
 import config from 'config';
 
-interface Config {
+export interface Config {
   env: string;
   http: HttpConfig;
+  logger: LoggerConfig;
 }
 
 interface HttpConfig {
   host: string;
   port: number;
 }
+export interface LoggerConfig {
+  format: LogFormat;
+}
 
-export function load(): Config {
+export function configLoad(): Config {
   try {
     return {
       env: config.get<string>('env'),
-      http: config.get<HttpConfig>('http')
+      http: config.get<HttpConfig>('http'),
+      logger: config.get<LoggerConfig>('logger')
     };
   } catch (e) {
     throw new Error(`failed to load config error: ${e}`);
